f07d932a3283524c08e7d82d15c1dc3550c54080
Georgin-j <52379659+Georgin-j@users.noreply.github.com>
2021-08-18 20:42:18 +0530

    Rocket Man

---
+++ Rocket Man/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float thrust = 1000f;
+    [SerializeField] float turn = 1f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessorThrust();
+        ProcessorRotation();
+    }
+    void ProcessorThrust()
+    { 
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(thrust * Time.deltaTime * Vector3.up);
+        }
+    }
+    void ProcessorRotation()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Rotation(turn);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Rotation(-turn);
+        }
+    }
+
+    void Rotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
